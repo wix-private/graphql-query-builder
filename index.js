06b3db1b283c2987a9fca8bf74ce029b91fab4c1
builduser <builduser@wix.com>
2016-07-31 12:53:55 +0000

    bower version 1.0.1

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 (function () {
 	'use strict';
 
-//=====================================================
-//============================ parce properties to find
-//=====================================================
+	//=====================================================
+	//============================ parce properties to find
+	//=====================================================
 
 	function parceFind(_levelA) {
 		//+++++++++++++++++++++++++++++++++++ work over Array
 		//++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-		let propsA = _levelA.map(function (currentValue, index) {
+		var propsA = _levelA.map(function (currentValue, index) {
 
-			let itemX = _levelA[index];
+			var itemX = _levelA[index];
 
 			if (itemX instanceof Query) {
 				return itemX.toString();
-			} else if (!Array.isArray(itemX) && "object" === typeof itemX) {
-				let propsA = Object.keys(itemX);
-				if (1 !== propsA.length) {
+			} else if (!Array.isArray(itemX) && "object" === (typeof itemX === "undefined" ? "undefined" : _typeof(itemX))) {
+				var _propsA = Object.keys(itemX);
+				if (1 !== _propsA.length) {
 					throw new RangeError("Alias objects should only have one value. was passed: " + JSON.stringify(itemX));
 				}
-				let propS = propsA[0];
-				let item = itemX[propS];
-				return `${propS} : ${item} `;
+				var propS = _propsA[0];
+				var item = itemX[propS];
+				return propS + " : " + item + " ";
 			} else if ("string" === typeof itemX) {
 				return itemX;
 			} else {
@@ -33,25 +37,26 @@
 		return propsA.join(",");
 	}
 
-//=====================================================
-//========================================= Query Class
-//=====================================================
+	//=====================================================
+	//========================================= Query Class
+	//=====================================================
 
 	function Query(_fnNameS, _aliasS_OR_Filter) {
+		var _this = this;
 
 		this.fnNameS = _fnNameS;
 		this.headA = [];
 
-		this.filter = (filtersO) => {
-			for (let propS in filtersO) {
-				this.headA.push(`${propS}:${("string" === typeof filtersO[propS]) ? JSON.stringify(filtersO[propS]) : filtersO[propS]}`);
+		this.filter = function (filtersO) {
+			for (var propS in filtersO) {
+				_this.headA.push(propS + ":" + ("string" === typeof filtersO[propS] ? JSON.stringify(filtersO[propS]) : filtersO[propS]));
 			}
-			return this;
+			return _this;
 		};
 
 		if ("string" === typeof _aliasS_OR_Filter) {
 			this.aliasS = _aliasS_OR_Filter;
-		} else if ("object" === typeof _aliasS_OR_Filter) {
+		} else if ("object" === (typeof _aliasS_OR_Filter === "undefined" ? "undefined" : _typeof(_aliasS_OR_Filter))) {
 			this.filter(_aliasS_OR_Filter);
 		} else if (undefined === _aliasS_OR_Filter && 2 == arguments.length) {
 			throw new TypeError("You have passed undefined as Second argument to 'Query'");
@@ -59,36 +64,37 @@
 			throw new TypeError("Second argument to 'Query' should be an alias name(String) or filter arguments(Object). was passed " + _aliasS_OR_Filter);
 		}
 
-		this.setAlias = (_aliasS) => {
-			this.aliasS = _aliasS;
-			return this
+		this.setAlias = function (_aliasS) {
+			_this.aliasS = _aliasS;
+			return _this;
 		};
 
-		this.find = function (findA) { // THIS NEED TO BE A "FUNCTION" to scope 'arguments'
+		this.find = function (findA) {
+			// THIS NEED TO BE A "FUNCTION" to scope 'arguments'
 			if (!findA) {
 				throw new TypeError("find value can not be >>falsy<<");
 			}
 			// if its a string.. it may have other values
 			// else it sould be an Object or Array of maped values
-			this.bodyS = parceFind((Array.isArray(findA)) ? findA : Array.from(arguments));
+			this.bodyS = parceFind(Array.isArray(findA) ? findA : Array.from(arguments));
 			return this;
-		}
+		};
 	};
 
-//=====================================================
-//===================================== Query prototype
-//=====================================================
+	//=====================================================
+	//===================================== Query prototype
+	//=====================================================
 
 	Query.prototype = {
 
-		toString: function () {
+		toString: function toString() {
 			if (undefined === this.bodyS) {
 				throw new ReferenceError("return properties are not defined. use the 'find' function to defined them");
 			}
 
-			return `${ (this.aliasS) ? (this.aliasS + ":") : "" } ${this.fnNameS } ${ (0 < this.headA.length) ? "(" + this.headA.join(",") + ")" : "" }  { ${ this.bodyS } }`;
+			return (this.aliasS ? this.aliasS + ":" : "") + " " + this.fnNameS + " " + (0 < this.headA.length ? "(" + this.headA.join(",") + ")" : "") + "  { " + this.bodyS + " }";
 		}
-	}
+	};
 
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 		module.exports = Query;
@@ -96,3 +102,4 @@
 		window.Query = Query;
 	}
 })();
+//# sourceMappingURL=index.js.map

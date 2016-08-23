ef297671f6b1fd0743509a092fcf8bbf8ba422da
Felix Mosheev <felixm@wix.com>
2016-08-23 16:10:09 +0300

    Refactor interface

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,74 @@
 class GraphQlQuery {
-	private headA;
-	private aliasS;
-	private bodyS;
+	private head;
+	private alias;
+	private body;
 
-	constructor(private fnNameS, private aliasS_OR_Filter) {
-		this.headA = [];
+	constructor(private fnName: string, private alias_OR_filter?: string|Object) {
+		this.head = [];
 
-		if ("string" === typeof aliasS_OR_Filter) {
-			this.aliasS = aliasS_OR_Filter;
-		} else if ("object" === typeof aliasS_OR_Filter) {
-			this.filter(aliasS_OR_Filter);
-		} else if (undefined === aliasS_OR_Filter && 2 == arguments.length) {
-			throw new TypeError("You have passed undefined as Second argument to 'GraphQlQuery'");
-		} else if (undefined !== aliasS_OR_Filter) {
-			throw new TypeError("Second argument to 'GraphQlQuery' should be an alias name(String) or filter arguments(Object). was passed " + aliasS_OR_Filter);
+		if (typeof alias_OR_filter === 'string') {
+			this.setAlias(alias_OR_filter);
+		} else if (typeof alias_OR_filter === 'object') {
+			this.filter(alias_OR_filter);
+		} else if (undefined === alias_OR_filter && 2 == arguments.length) {
+			throw new TypeError(`You have passed undefined as Second argument to 'GraphQlQuery'`);
+		} else if (undefined !== alias_OR_filter) {
+			throw new TypeError(`Second argument to 'GraphQlQuery' should be an alias name(String) or filter arguments(Object). was passed ${alias_OR_filter}`);
 		}
 	}
 
-	public filter(filtersO: any): GraphQlQuery {
-		for (let propS in filtersO) {
-			this.headA.push(`${propS}:${("string" === typeof filtersO[propS]) ? JSON.stringify(filtersO[propS]) : filtersO[propS]}`);
+	public filter(filters: Object): GraphQlQuery {
+		for (let props in filters) {
+			this.head.push(`${props}:${(typeof filters[props] === 'string') ? JSON.stringify(filters[props]) : filters[props]}`);
 		}
 		return this;
 	}
 
-	public setAlias(aliasS: string): GraphQlQuery {
-		this.aliasS = aliasS;
+	public setAlias(alias: string): GraphQlQuery {
+		this.alias = alias;
 		return this;
 	}
 
-	public find(findA: any): GraphQlQuery {
-		if (!findA) {
-			throw new TypeError("find value can not be >>falsy<<");
+	public find(find: string|Array<string>): GraphQlQuery {
+		if (!find) {
+			throw new TypeError('find value can not be >>falsy<<');
 		}
-		this.bodyS = this.parseFind((Array.isArray(findA)) ? findA : Array.prototype.slice.call(arguments));
+		this.body = this.parseFind((Array.isArray(find)) ? find : Array.prototype.slice.call(arguments));
 
 		return this;
 	}
 
-	public parseFind(levelA: Array<any>): string {
+	public toString(): string {
+		if (undefined === this.body) {
+			throw new ReferenceError(`return properties are not defined. use the 'find' function to defined them`);
+		}
+
+		return `${ (this.alias) ? (this.alias + ":") : "" } ${this.fnName } ${ (0 < this.head.length) ? "(" + this.head.join(",") + ")" : "" }  { ${ this.body } }`;
+	}
+
+	private parseFind(levelA: Array<any>): string {
 		let propsA = levelA.map(function (currentValue, index) {
 
 			let itemX = levelA[index];
 
 			if (itemX instanceof GraphQlQuery) {
 				return itemX.toString();
-			} else if (!Array.isArray(itemX) && "object" === typeof itemX) {
+			} else if (!Array.isArray(itemX) && typeof itemX === 'object') {
 				let propsA = Object.keys(itemX);
 				if (1 !== propsA.length) {
-					throw new RangeError("Alias objects should only have one value. was passed: " + JSON.stringify(itemX));
+					throw new RangeError(`Alias objects should only have one value. was passed: ${JSON.stringify(itemX)}`);
 				}
 				let propS = propsA[0];
 				let item = itemX[propS];
 				return `${propS} : ${item} `;
-			} else if ("string" === typeof itemX) {
+			} else if (typeof itemX === 'string') {
 				return itemX;
 			} else {
-				throw new RangeError("cannot handle Find value of " + itemX);
+				throw new RangeError(`cannot handle Find value of ${itemX}`);
 			}
 		});
 
-		return propsA.join(",");
-	}
-
-	public toString(): string {
-		if (undefined === this.bodyS) {
-			throw new ReferenceError("return properties are not defined. use the 'find' function to defined them");
-		}
-
-		return `${ (this.aliasS) ? (this.aliasS + ":") : "" } ${this.fnNameS } ${ (0 < this.headA.length) ? "(" + this.headA.join(",") + ")" : "" }  { ${ this.bodyS } }`;
+		return propsA.join(',');
 	}
 }
 

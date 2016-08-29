621007d98d8102671eafc4676b529d03075ea8dd
builduser <builduser@wix.com>
2016-08-29 12:48:41 +0000

    bower version 1.0.124

---
+++ GraphQlQuery.d.ts
@@ -0,0 +1,35 @@
+declare namespace gql {
+    interface GraphQlQueryFactory {
+        new (fnName: string | IAlias, argumentsMap?: IArgumentsMap): GraphQlQuery;
+    }
+    interface IArgumentsMap {
+        [index: string]: string | number | boolean | Object;
+    }
+    interface IAlias {
+        [index: string]: string;
+    }
+    interface IHead {
+        fnName: IAlias;
+        argumentsMap?: IArgumentsMap;
+    }
+    interface IBody {
+        attr: IAlias;
+        argumentsMap?: IArgumentsMap;
+    }
+    interface ISelection extends IArgumentsMap {
+        _filter?: Object;
+    }
+    class GraphQlQuery {
+        private head;
+        private body;
+        private isContainer;
+        constructor(fnName: string | IAlias, argumentsMap?: IArgumentsMap);
+        select(...selects: (string | ISelection)[]): GraphQlQuery;
+        join(...queries: GraphQlQuery[]): GraphQlQuery;
+        toString(): string;
+        private buildHeader();
+        private handleArguments(argumentsMap);
+        private handleAlias(attr);
+        private buildBody();
+    }
+}
---
+++ GraphQlQuery.js
@@ -0,0 +1,90 @@
+var gql;
+(function (gql) {
+    var GraphQlQuery = (function () {
+        function GraphQlQuery(fnName, argumentsMap) {
+            if (argumentsMap === void 0) { argumentsMap = {}; }
+            this.head = typeof fnName === 'string' ? { fnName: (_a = {}, _a[fnName] = fnName, _a) } : { fnName: fnName };
+            this.head.argumentsMap = argumentsMap;
+            this.body = [];
+            this.isContainer = false;
+            var _a;
+        }
+        GraphQlQuery.prototype.select = function () {
+            var selects = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                selects[_i - 0] = arguments[_i];
+            }
+            if (this.isContainer) {
+                throw new Error('Can`t use selection on joined query.');
+            }
+            this.body = this.body.concat(selects.map(function (item) {
+                var selection = {};
+                if (typeof item === 'string') {
+                    selection.attr = (_a = {}, _a[item] = item, _a);
+                    selection.argumentsMap = {};
+                }
+                else if (typeof item === 'object') {
+                    selection.argumentsMap = item['_filter'] || {};
+                    delete item['_filter'];
+                    selection.attr = item;
+                }
+                return selection;
+                var _a;
+            }));
+            return this;
+        };
+        GraphQlQuery.prototype.join = function () {
+            var queries = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                queries[_i - 0] = arguments[_i];
+            }
+            var combined = new GraphQlQuery('');
+            combined.isContainer = true;
+            combined.body.push(this);
+            combined.body = combined.body.concat(queries);
+            return combined;
+        };
+        GraphQlQuery.prototype.toString = function () {
+            return this.isContainer ? "{ " + this.buildBody() + " }" : "{ " + this.buildHeader() + "{" + this.buildBody() + "} }";
+        };
+        GraphQlQuery.prototype.buildHeader = function () {
+            return this.handleAlias(this.head.fnName) + this.handleArguments(this.head.argumentsMap);
+        };
+        GraphQlQuery.prototype.handleArguments = function (argumentsMap) {
+            var hasArguments = Object.keys(argumentsMap).length > 0;
+            var args = '';
+            if (hasArguments) {
+                args = Object.keys(argumentsMap)
+                    .map(function (key) { return (key + ": " + JSON.stringify(argumentsMap[key])); })
+                    .join(', ');
+                args = "(" + args + ")";
+            }
+            return args;
+        };
+        GraphQlQuery.prototype.handleAlias = function (attr) {
+            var alias = Object.keys(attr)[0];
+            var value = attr[alias];
+            value = (alias !== value) ? alias + ": " + value : value;
+            return value;
+        };
+        GraphQlQuery.prototype.buildBody = function () {
+            var _this = this;
+            return this.body.map(function (item) {
+                if (_this.isContainer) {
+                    var content = item.toString();
+                    return content.substr(2, content.length - 4);
+                }
+                else {
+                    return _this.handleAlias(item['attr']) + _this.handleArguments(item['argumentsMap']);
+                }
+            }).join(' ');
+        };
+        return GraphQlQuery;
+    }());
+    gql.GraphQlQuery = GraphQlQuery;
+})(gql || (gql = {}));
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = gql.GraphQlQuery;
+}
+
+//# sourceMappingURL=GraphQlQuery.js.map
